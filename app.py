60ac2b7b115f3bbaf54cf9812b8b5a7c129992ae
Radoslav Dugas / MAIN <dugas.radoslav@gmail.com>
2022-07-10 17:49:24 +0200

    Idem na burger

---
+++ app.py
@@ -0,0 +1,21 @@
+from crypt import methods
+import imp
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+
+class LoginForm(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('password')
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'SecretKey'
+
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
+        form = LoginForm
+
+        return render_template('index.html', form=form)
+
+    return app
